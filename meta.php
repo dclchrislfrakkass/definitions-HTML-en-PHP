c3e65f79716083acff04c9a224b7137cc4d677b7
ChrisL_frakkass <43646777+dclchrislfrakkass@users.noreply.github.com>
2018-11-06 15:47:20 +0100

    change files into php

---
+++ meta.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="css/style.css">
+     <title>Définitions des Balises html</title>
+
+</head>
+
+<header>
+    <!--maj by sicca--><!--review by chris --><!--re-review by sicca-->
+    <h1 id="navigation"><a href="index.html" target="_self">Accueil </a></h1>
+    <ul id="navigation">
+        <li><a href="body.html" target="_self">BODY</a></li>
+        <li><a href="title.html" target="_self">TITLE</a></li>
+        <li><a href="embed.html" target="_self">B, I, U, PRE</a></li>
+        <li><a href="dot.html" target="_self">P, UL, LI, BR, HR</a></li>
+        <li><a href="table.html" target="_self">TABLE TD TR</a></li>
+        <li><a href="img.html" target="_self">IMG</a></li>
+        <li><a href="html.html" target="_self">HTML</a></li>
+        <li><a href="links.html" target="_self">A</a></li>
+        <li> <a href="h.html" target="_self">H1,H2,H3,...</a></li>
+        <li><a href="com.html" target="_self">!…</a></li>
+        <li><a href="form.html" target="_self">FORM</a></li>
+        <li><a href="input.html" target="_self">INPUT</a></li>
+        <li><a href="textarea.html" target="_self">TEXTAREA</a></li>
+        <li><a href="select.html" target="_self">SELECT</a></li>
+        <li id="en-cours"><a href="meta.html" target="_self">META</a></li>
+        <li><a href="head.html" target="_self">HEAD</a></li>
+        <li><a href="link.html" target="_self">LINK</a></li>
+        <li><a href="div.html" target="_self">DIV</a></li>
+        <li><a href="footer.html" target="_self">FOOTER</a></li>
+    </ul>   
+</header>
+
+<body>
+    <section>
+            <article class="definition">
+        <h1>META</h1>
+<h1><em>Définition:</em></h1>
+<p>
+<em>La balise <strong>META</strong> sont les informations relatives à la page.</em><br>
+<em>Les données du Metadata ne sont pas affichés sur la page mais sont "traduit" par la machine.</em><br>
+<em>Les éléments du meta sont généralement la description de la page, les mots clés, l'auteur de la page, la date de dernière modification...</em><br>
+<em>Les metadatas peuvent être utilisé par les navigateurs lui donnant les informations sur le contenu, par les moteurs de recherche mais également d'autres services en lignes.</em><br>
+
+</p>    
+            </article>
+
+            <article class="exemple">
+<h1><em>Exemple:</em></h1>
+<p>
+    <em><xmp>    
+Afficher des mots clés pour les moteurs de recherche:
+<meta name="keywords" content="HTML, CSS, XML, XHTML, JavaScript">
+
+Décrire sa page :
+<meta name="description" content="Free Web tutorials on HTML and CSS">
+
+Raffraichir la page toutes les 30s:
+<meta http-equiv="refresh" content="30">
+
+
+    </xmp></em>
+</p>
+            </article>
+    </section>
+</body>
+
+
+<footer>
+        <!---table by chris-->
+        <table>
+            <tr>
+                <th>Créer par</th>
+                <th>Sources</th>
+            </tr>
+            <tr>
+                <td><a href="https://github.com/dclsifufrancis" target="_blank">dclSifuFrancis</a>,<a href="https://github.com/dcl-sicca" target="_blank"> dclSicca</a>,<a href="https://github.com/dclchrislfrakkass" target="_blank"> dclChrisFrakkass</a>. <br><br>
+                    <img src="./images/logo _Sifu_Francis_256.png" alt="dclfrancis"/><img src="./images/avatar_sicca_256.png" alt="dclsicca"/><img src="./images/logo256.png" alt="dclfrakkass"/></td>
+                <td><a href="https://www.w3schools.com/" target="_blank">W3 School</a> 
+                    <br> 
+                    <a href="https://fr.wikipedia.org" target="_blank"> wikipédia</a>.</td>
+            </tr>
+        </table>
+    
+    
+    <!---   old by chris 
+     Créer par <a href="https://github.com/dclsifufrancis" target="_blank">dclSifuFrancis</a>,<a href="https://github.com/dcl-sicca" target="_blank"> dclSicca</a>,<a href="https://github.com/dclchrislfrakkass" target="_blank"> dclChrisFrakkass</a>. <br><br>
+    <img src="./images/logo _Sifu_Francis_256.png" alt="dclfrancis"/><img src="./images/avatar_sicca_256.png" alt="dclsicca"/><img src="./images/logo256.png" alt="dclfrakkass"/>
+    <article>sources: <a href="https://www.w3schools.com/" target="_blank">W3 School</a> et <a href="https://fr.wikipedia.org" target="_blank"> wikipédia</a>.</article>
+    
+    ---->
+    </footer>
+</html>
